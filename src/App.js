9497ae1f0148d951ca6622e64c257b96d08564d5
Yaghs <syaghinloo@gmail.com>
2026-01-11 20:12:52 -0500

    added a section for game status with styling (currently it only shows you win)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word within 8 attempts to keep the program world safe from assembly!</p>
       </header>
+      <section className="game-status">
+        <h2>You Win!</h2>
+        <p>Well done!</p>
+      </section>
     </main>
   );
 }
