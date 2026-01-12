d88ed75be2b40d0eba7c0b887db247d71a49e216
Yaghs <syaghinloo@gmail.com>
2026-01-11 20:12:52 -0500

    added an array of languages with custom css
    mapped through each element and returned it as a span rendering each language name and their own css
    
    refactored the css for the rest of the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 
-
+import {languages} from "./languages.js"
 function App() {
+
+  const languagesList = languages.map((language, index)=>{
+    return(
+      <span key={index} className="language-badge"
+       style={{backgroundColor: language.backgroundColor, color: language.color}}>{language.name}
+       </span>
+    )
+  })
+
   return (
     <main>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>Guess the word within 8 attempts to keep the program world safe from assembly!</p>
       </header>
+      <section className="game-status">
+        <h2>You Win!</h2>
+        <p>Well done!</p>
+      </section>
+      <section className="language-section">
+        {languagesList}
+      </section>
+      
     </main>
   );
 }
