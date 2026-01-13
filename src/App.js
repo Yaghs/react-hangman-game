03b7cd5ab22166f209c55a62691e68f38ba86305
Yaghs <syaghinloo@gmail.com>
2026-01-13 15:22:12 -0500

    add clsx for dynamic button styling and implement keyboard for letter guessing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
 
 import {languages} from "./languages.js"
 import {useState} from "react"
+import {clsx} from "clsx"
 
 function App() {
-  const [currentWord, setCurrentWord] = useState("Hello")
+  const [secretWord, setSecretWord] = useState("hello")
+  const [guessedLetters, setguessedLetters] = useState([])
+  console.log(guessedLetters)
 
-  const currentLetters = currentWord.split("").map((letter, index) =>(<span key={index}>{letter.toUpperCase()}</span>))
 
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  function addGuestLetterHandler(letter){
+    setguessedLetters(prevLetter => 
+      [...prevLetter, letter]
+    )
+   if(secretWord.includes(letter)){
+    console.log("correct")
+   }
+   else{
+    console.log("incorrect")
+   }
+   console.log(guessedLetters)
+   console.log(letter)
+  }
+  //TODO: the letters are all orange, the background colors should only change IF the user has clicked on it
+  // if the letter is correct, then set the background color to green if not, then set it to red 
+
+  const currentLetters = secretWord.split("").map((letter, index) =>(<span key={index}>{letter.toUpperCase()}</span>))
+
+  const keyBoard = alphabet.split("").map((letter, index) => {
+    /**
+     * check if the selected letter is included inside the guessedletters AND secretWord
+     * 
+     * if the previous condition is true, change the backgroundColor of the button to be green
+     * 
+     * if the selected letter is included inside the guessedletters but not in secretWord, change the backgroundColor to red 
+     * 
+     * if its not in guessed letter and not in secretWord then keep the backgroundColor in its default state
+     */
+    const isInSecretWord = secretWord.includes(letter) 
+    const isInGuessedLetters = guessedLetters.includes(letter)
+    let isCorrect = false
+    let isWrong = false
+    if(isInSecretWord && isInGuessedLetters){
+      isCorrect = true
+    }
+    else if(!isInSecretWord && isInGuessedLetters){
+      isWrong = true
+    }
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong
+    })
+    return (<button className={className}
+                    key={index} 
+                    onClick = {() => {addGuestLetterHandler(letter)}} >
+                      {letter.toUpperCase()}
+            </button>)
+  })
+    
+    
+   
 
   const languagesList = languages.map((language, index)=>{
     return(
@@ -31,6 +87,9 @@ function App() {
       </section>
       <section className="word">
         {currentLetters}
+      </section>
+      <section className="keyboard">
+        {keyBoard}
       </section>
     </main>
   );
